9734727cb9cc1af97965bf04ddba3f52778da389
Murilo Dionizio <murilomelodionizio@gmail.com>
2026-04-28 16:22:47 -0300

    Criando e estilizando rota de adicionar produtos

--- main.py
+++ main.py
@@ -18,13 +18,12 @@ def index(request:Request):
     )
 
 @app.get("/eletronico/cadastro")
-def exibir_cadastro(request:Request):
+def exibir_cadastro(request: Request, db: Session = Depends(get_db)):
+    categorias = db.query(Eletronico).all()
     return templates.TemplateResponse(
         request,
         "cadastrar.html",
-        {"request":request}
-    )
-
+        {"request": request, "categorias": categorias })
 @app.post("/eletronico")
 def criar_eletronico(
     nome: str = Form(...),
@@ -33,4 +32,22 @@ def criar_eletronico(
     novo_eletronico = Eletronico(nome=nome)
     db.add(novo_eletronico)
     db.commit()
-    return RedirectResponse(url="/", status_code=303)
+    return RedirectResponse(url="/eletronico/cadastro", status_code=303)
+
+@app.post("/produto/salvar")
+def salvar_produto(
+    nome: str = Form(...),
+    descricao: str = Form(...),
+    eletronico_id: int = Form(...),
+    db: Session = Depends(get_db)
+):
+
+    novo_produto = Produto(
+        nome=nome, 
+        descricao=descricao, 
+        eletronico_id=eletronico_id
+    )
+    
+    db.add(novo_produto)
+    db.commit()
+    return RedirectResponse(url="/produto_criado", status_code=303)
